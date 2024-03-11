9d81beae8f85fc09dce5eedea33fc8652a58d006
Roman <ro.zhigaikov@ined.ru>
2024-03-11 18:38:43 +0300

    0.0.2

--- src/components/TableForGM.vue
+++ src/components/TableForGM.vue
@@ -5,11 +5,21 @@
 		savingThrows: {{ mainDataComputed.savingThrows }}
 		<br>
 		perception: {{ mainDataComputed.perception }}
+		<br>
+		hp: {{ mainDataComputed.hp }}
+		<br>
+		attack: {{ mainDataComputed.attack }}
+		<br>
+		damage: {{ mainDataComputed.damage }}
+		<br>
+		spells: {{ mainDataComputed.spells }}
+		<br>
+		aoe: {{ mainDataComputed.aoe }}
 	</div>
 </template>
 
 <script setup lang="ts">
-import {useTablesStore} from '@/store/tables';
+import {useTablesStore} from '@/store/tables.ts';
 import {computed, ref} from "vue";
 
 const tablesStore = useTablesStore();
@@ -20,12 +30,16 @@ const mainDataComputed = computed(() => {
 	return {
 		ac: tablesStore.acTable.find(el => el.level === currentLevel.value),
 		savingThrows: tablesStore.savingThrowsTable.find(el => el.level === currentLevel.value),
-		perception: tablesStore.perceptionTable.find(el => el.level === currentLevel.value)
+		perception: tablesStore.perceptionTable.find(el => el.level === currentLevel.value),
+		hp: tablesStore.hpTable.find(el => el.level === currentLevel.value),
+		attack: tablesStore.attackTable.find(el => el.level === currentLevel.value),
+		damage: tablesStore.damageTable.find(el => el.level === currentLevel.value),
+		spells: tablesStore.spellsTable.find(el => el.level === currentLevel.value),
+		aoe: tablesStore.aoeTable.find(el => el.level === currentLevel.value),
 	}
 })
 
 tablesStore.getAcTable();
-tablesStore.getSavingThrowsTable();
 
 </script>
 
--- src/store/tables.ts
+++ src/store/tables.ts
@@ -6,22 +6,23 @@ export const useTablesStore = defineStore('tables', () => {
   const acTable = ref([]);
   const savingThrowsTable = ref([]);
   const perceptionTable = ref([]);
+  const hpTable = ref([]);
+  const attackTable = ref([]);
+  const damageTable = ref([]);
+  const spellsTable = ref([]);
+  const aoeTable = ref([]);
 
   function getAcTable(): Promise<void> {
-    return axios.get(`https://65d7132527d9a3bc1d7a1610.mockapi.io/armor-class`)
+    return axios.get(`https://65d7132527d9a3bc1d7a1610.mockapi.io/getTables`)
       .then((res) => {
-        acTable.value = res.data
-      })
-      .catch((rej) => {
-        console.log(rej)
-      })
-  }
-
-  function getSavingThrowsTable(): Promise<void> {
-    return axios.get(`https://65d7132527d9a3bc1d7a1610.mockapi.io/saving-throws`)
-      .then((res) => {
-        savingThrowsTable.value = res.data
-        perceptionTable.value = res.data
+        acTable.value = res.data.find(el => el.type === 'ac').data
+        savingThrowsTable.value = res.data.find(el => el.type === 'saving-throws').data
+        perceptionTable.value = res.data.find(el => el.type === 'saving-throws').data
+        hpTable.value = res.data.find(el => el.type === 'hp').data
+        attackTable.value = res.data.find(el => el.type === 'attack').data
+        damageTable.value = res.data.find(el => el.type === 'damage').data
+        spellsTable.value = res.data.find(el => el.type === 'spells').data
+        aoeTable.value = res.data.find(el => el.type === 'aoe').data
       })
       .catch((rej) => {
         console.log(rej)
@@ -32,7 +33,11 @@ export const useTablesStore = defineStore('tables', () => {
     acTable,
     savingThrowsTable,
     perceptionTable,
+    hpTable,
+    attackTable,
+    damageTable,
+    spellsTable,
+    aoeTable,
     getAcTable,
-    getSavingThrowsTable
   }
 })
