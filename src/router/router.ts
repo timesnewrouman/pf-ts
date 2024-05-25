dc84c44c856ccdeb63a08185836c7d4c71cf26f8
Roman <ro.zhigaikov@ined.ru>
2024-05-25 12:26:54 +0300

    gh-pages

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,14 +1,14 @@
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'MainPage',
-  //   component: () => import("@/pages/MainPage.vue"),
-  // },
   {
     path: '/',
-    name: 'VuetifyTest',
-    component: () => import("@/pages/VuetifyTest.vue"),
+    name: 'MainPage',
+    component: () => import("@/pages/MainPage.vue"),
   },
+  // {
+  //   path: '/',
+  //   name: 'VuetifyTest',
+  //   component: () => import("@/pages/VuetifyTest.vue"),
+  // },
 ]
 
 export {routes};
